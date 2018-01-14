c8ae2bafbed8148b4a009cb12c8af31141b9e57a
fenric <afenric@gmail.com>
2018-01-15 01:35:38 +0300

    Рефакторинг.

--- classes/Response.php
+++ classes/Response.php
@@ -2,8 +2,8 @@
 /**
  * It's free open-source software released under the MIT License.
  *
- * @author Anatoly Fenric <a.fenric@gmail.com>
- * @copyright Copyright (c) 2013-2017 by Fenric Laboratory
+ * @author Anatoly Fenric <anatoly.fenric@gmail.com>
+ * @copyright Copyright (c) 2013-2018 by Fenric Laboratory
  * @license https://github.com/fenric/framework/blob/master/LICENSE.md
  * @link https://github.com/fenric/framework
  */
@@ -17,9 +17,86 @@ class Response
 {
 
 	/**
+	 * List of HTTP status codes
+	 */
+	public const STATUS_100 = [100, 'Continue'];
+	public const STATUS_101 = [101, 'Switching Protocols'];
+	public const STATUS_102 = [102, 'Processing'];
+	public const STATUS_103 = [103, 'Early Hints'];
+	public const STATUS_200 = [200, 'OK'];
+	public const STATUS_201 = [201, 'Created'];
+	public const STATUS_202 = [202, 'Accepted'];
+	public const STATUS_203 = [203, 'Non-Authoritative Information'];
+	public const STATUS_204 = [204, 'No Content'];
+	public const STATUS_205 = [205, 'Reset Content'];
+	public const STATUS_206 = [206, 'Partial Content'];
+	public const STATUS_207 = [207, 'Multi-Status'];
+	public const STATUS_208 = [208, 'Already Reported'];
+	public const STATUS_226 = [226, 'IM Used'];
+	public const STATUS_300 = [300, 'Multiple Choices'];
+	public const STATUS_301 = [301, 'Moved Permanently'];
+	public const STATUS_302 = [302, 'Found'];
+	public const STATUS_303 = [303, 'See Other'];
+	public const STATUS_304 = [304, 'Not Modified'];
+	public const STATUS_305 = [305, 'Use Proxy'];
+	public const STATUS_306 = [306, 'Switch Proxy'];
+	public const STATUS_307 = [307, 'Temporary Redirect'];
+	public const STATUS_308 = [308, 'Permanent Redirect'];
+	public const STATUS_400 = [400, 'Bad Request'];
+	public const STATUS_401 = [401, 'Unauthorized'];
+	public const STATUS_402 = [402, 'Payment Required'];
+	public const STATUS_403 = [403, 'Forbidden'];
+	public const STATUS_404 = [404, 'Not Found'];
+	public const STATUS_405 = [405, 'Method Not Allowed'];
+	public const STATUS_406 = [406, 'Not Acceptable'];
+	public const STATUS_407 = [407, 'Proxy Authentication Required'];
+	public const STATUS_408 = [408, 'Request Timeout'];
+	public const STATUS_409 = [409, 'Conflict'];
+	public const STATUS_410 = [410, 'Gone'];
+	public const STATUS_411 = [411, 'Length Required'];
+	public const STATUS_412 = [412, 'Precondition Failed'];
+	public const STATUS_413 = [413, 'Payload Too Large'];
+	public const STATUS_414 = [414, 'URI Too Long'];
+	public const STATUS_415 = [415, 'Unsupported Media Type'];
+	public const STATUS_416 = [416, 'Range Not Satisfiable'];
+	public const STATUS_417 = [417, 'Expectation Failed'];
+	public const STATUS_418 = [418, 'I\'m a teapot'];
+	public const STATUS_421 = [421, 'Misdirected Request'];
+	public const STATUS_422 = [422, 'Unprocessable Entity'];
+	public const STATUS_423 = [423, 'Locked'];
+	public const STATUS_424 = [424, 'Failed Dependency'];
+	public const STATUS_426 = [426, 'Upgrade Required'];
+	public const STATUS_428 = [428, 'Precondition Required'];
+	public const STATUS_429 = [429, 'Too Many Requests'];
+	public const STATUS_431 = [431, 'Request Header Fields Too Large'];
+	public const STATUS_444 = [444, 'No Response'];
+	public const STATUS_451 = [451, 'Unavailable For Legal Reasons'];
+	public const STATUS_495 = [495, 'SSL Certificate Error'];
+	public const STATUS_496 = [496, 'SSL Certificate Required'];
+	public const STATUS_497 = [497, 'HTTP Request Sent to HTTPS Port'];
+	public const STATUS_499 = [499, 'Client Closed Request'];
+	public const STATUS_500 = [500, 'Internal Server Error'];
+	public const STATUS_501 = [501, 'Not Implemented'];
+	public const STATUS_502 = [502, 'Bad Gateway'];
+	public const STATUS_503 = [503, 'Service Unavailable'];
+	public const STATUS_504 = [504, 'Gateway Timeout'];
+	public const STATUS_505 = [505, 'HTTP Version Not Supported'];
+	public const STATUS_506 = [506, 'Variant Also Negotiates'];
+	public const STATUS_507 = [507, 'Insufficient Storage'];
+	public const STATUS_508 = [508, 'Loop Detected'];
+	public const STATUS_510 = [510, 'Not Extended'];
+	public const STATUS_511 = [511, 'Network Authentication Required'];
+
+	/**
 	 * Status of the response
 	 */
-	protected $status = 200;
+	protected $statusCode = 200;
+	protected $statusMessage = 'OK';
+
+	/**
+	 * Charset of the response
+	 */
+	protected $charset = 'UTF-8';
 
 	/**
 	 * Headers of the response
@@ -34,34 +111,107 @@ class Response
 	/**
 	 * Content of the response
 	 */
-	protected $content = '';
+	protected $content = null;
 
 	/**
 	 * Sets the response status
 	 */
-	public function setStatus(int $status) : self
+	public function status(array $status) : self
 	{
-		$this->status = $status;
+		list($code, $message) = $status;
+
+		$this->statusCode = $code;
+		$this->statusMessage = $message;
 
 		return $this;
 	}
 
 	/**
-	 * Gets the response status
+	 * Sets the response charset
 	 */
-	public function getStatus() : int
+	public function charset(string $charset) : self
 	{
-		return $this->status;
+		$this->charset = $charset;
+
+		return $this;
 	}
 
 	/**
 	 * Sets the response header
 	 */
-	public function setHeader(string $header) : self
+	public function header(string $name, ?string $value, bool $replace = true) : self
 	{
-		$this->headers[] = $header;
+		$this->headers[] = [$name, $value, $replace];
 
 		return $this;
+	}
+
+	/**
+	 * Sets the response cookie
+	 */
+	public function cookie(string $name, ?string $value, int $lifetime = 0, array $options = []) : self
+	{
+		$this->cookies[] = [$name, $value, $lifetime, $options];
+
+		return $this;
+	}
+
+	/**
+	 * Sets the response content
+	 */
+	public function content(?string $content) : self
+	{
+		$this->content = $content;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the response content as JSON
+	 */
+	public function json($data, ...$options) : self
+	{
+		$this->header('Content-Type', sprintf('application/json; charset=%s', $this->charset));
+
+		$this->content(json_encode($data, ...$options));
+
+		return $this;
+	}
+
+	/**
+	 * Sets the response content as view
+	 */
+	public function view(string $name, array $variables = []) : self
+	{
+		$this->header('Content-Type', sprintf('text/html; charset=%s', $this->charset));
+
+		$this->content(fenric()->callSharedService('view', [$name])->render($variables));
+
+		return $this;
+	}
+
+	/**
+	 * Gets the response status code
+	 */
+	public function getStatusCode() : int
+	{
+		return $this->statusCode;
+	}
+
+	/**
+	 * Gets the response status message
+	 */
+	public function getStatusMessage() : string
+	{
+		return $this->statusMessage;
+	}
+
+	/**
+	 * Gets the response charset
+	 */
+	public function getCharset() : string
+	{
+		return $this->charset;
 	}
 
 	/**
@@ -73,20 +223,6 @@ class Response
 	}
 
 	/**
-	 * Sets the response cookie
-	 */
-	public function setCookie(string $name, string $value, int $lifetime = 0, array $options = []) : self
-	{
-		if ($lifetime <> 0) {
-			$lifetime += time();
-		}
-
-		$this->cookies[] = [$name, $value, $lifetime, $options];
-
-		return $this;
-	}
-
-	/**
 	 * Gets the response cookies
 	 */
 	public function getCookies() : array
@@ -95,45 +231,91 @@ class Response
 	}
 
 	/**
-	 * Sets the response content
-	 */
-	public function setContent(string $content) : self
-	{
-		$this->content = $content;
-
-		return $this;
-	}
-
-	/**
-	 * Sets the response content as JSON
-	 */
-	public function setJSON($data, int $options = 0, string $charset = 'UTF-8') : self
-	{
-		$this->setHeader(sprintf('Content-Type: application/json; charset=%s', $charset));
-
-		$this->setContent(json_encode($data, $options));
-
-		return $this;
-	}
-
-	/**
-	 * Sets the response content as view
-	 */
-	public function setView(string $name, array $variables = [], string $charset = 'UTF-8') : self
-	{
-		$this->setHeader(sprintf('Content-Type: text/html; charset=%s', $charset));
-
-		$this->setContent(fenric()->callSharedService('view', [$name])->render($variables));
-
-		return $this;
-	}
-
-	/**
 	 * Gets the response content
 	 */
-	public function getContent() : string
+	public function getContent() :? string
 	{
 		return $this->content;
+	}
+
+	/**
+	 * Checks whether the response status code is invalid
+	 */
+	public function isInvalid() : bool
+	{
+		return ($this->getStatusCode() < 100 || $this->getStatusCode() >= 600);
+	}
+
+	/**
+	 * Checks whether the response status code is informational
+	 */
+	public function isInformational() : bool
+	{
+		return ($this->getStatusCode() >= 100 && $this->getStatusCode() < 200);
+	}
+
+	/**
+	 * Checks whether the response status code is successful
+	 */
+	public function isSuccessful() : bool
+	{
+		return ($this->getStatusCode() >= 200 && $this->getStatusCode() < 300);
+	}
+
+	/**
+	 * Checks whether the response status code is redirection
+	 */
+	public function isRedirection() : bool
+	{
+		return ($this->getStatusCode() >= 300 && $this->getStatusCode() < 400);
+	}
+
+	/**
+	 * Checks whether the response status code indicates a client error
+	 */
+	public function isClientError() : bool
+	{
+		return ($this->getStatusCode() >= 400 && $this->getStatusCode() < 500);
+	}
+
+	/**
+	 * Checks whether the response status code indicates a server error
+	 */
+	public function isServerError() : bool
+	{
+		return ($this->getStatusCode() >= 500 && $this->getStatusCode() < 600);
+	}
+
+	/**
+	 * Checks whether the response is OK
+	 */
+	public function isOk() : bool
+	{
+		return $this->getStatusCode() === 200;
+	}
+
+	/**
+	 * Checks whether the response is forbidden error
+	 */
+	public function isForbidden() : bool
+	{
+		return $this->getStatusCode() === 403;
+	}
+
+	/**
+	 * Checks whether the response is page not found error
+	 */
+	public function isNotFound() : bool
+	{
+		return $this->getStatusCode() === 404;
+	}
+
+	/**
+	 * Checks whether the response is empty
+	 */
+	public function isEmpty() : bool
+	{
+		return in_array($this->getStatusCode(), [204, 304]);
 	}
 
 	/**
@@ -141,63 +323,51 @@ class Response
 	 */
 	public function send() : void
 	{
-		if (fenric('event::http.response.before.send')->run([$this]))
+		http_response_code(
+			$this->getStatusCode()
+		);
+
+		if (count($this->getHeaders()) > 0)
 		{
-			if (fenric('event::http.response.before.send.status')->run([$this, & $this->status]))
+			foreach ($this->getHeaders() as $header)
 			{
-				http_response_code($this->getStatus());
+				list($name, $value, $replace) = $header;
 
-				fenric('event::http.response.after.send.status')->run([$this, $this->status]);
-			}
-
-			if (count($this->getHeaders()) > 0)
-			{
-				if (fenric('event::http.response.before.send.headers')->run([$this, & $this->headers]))
+				if ($value === null)
 				{
-					foreach ($this->getHeaders() as $header)
-					{
-						if (fenric('event::http.response.before.send.header')->run([$this, & $header]))
-						{
-							header($header, true, $this->getStatus());
+					header_remove($name);
 
-							fenric('event::http.response.after.send.header')->run([$this, $header]);
-						}
-					}
-
-					fenric('event::http.response.after.send.headers')->run([$this, $this->headers]);
+					continue;
 				}
-			}
 
-			if (count($this->getCookies()) > 0)
+				header(sprintf('%s: %s', $name, $value), $replace);
+			}
+		}
+
+		if (count($this->getCookies()) > 0)
+		{
+			foreach ($this->getCookies() as $cookie)
 			{
-				if (fenric('event::http.response.before.send.cookies')->run([$this, & $this->cookies]))
+				list($name, $value, $lifetime, $options) = $cookie;
+
+				if ($value === null)
 				{
-					foreach ($this->getCookies() as $cookie)
-					{
-						if (fenric('event::http.response.before.send.cookie')->run([$this, & $cookie]))
-						{
-							list($name, $value, $lifetime, $options) = $cookie;
-
-							extract($options + fenric('config::cookies')->all(), EXTR_OVERWRITE);
-
-							setcookie($name, $value, $lifetime, $path, $domain, $httpsOnly, $httpOnly);
-
-							fenric('event::http.response.after.send.cookie')->run([$this, $cookie]);
-						}
-					}
-
-					fenric('event::http.response.after.send.cookies')->run([$this, $this->cookies]);
+					$lifetime = time() - 3600;
 				}
+				else if ($lifetime <> 0)
+				{
+					$lifetime += time();
+				}
+
+				extract($options + fenric('config::cookies')->all(), EXTR_OVERWRITE);
+
+				setcookie($name, $value, $lifetime, $path, $domain, $httpsOnly, $httpOnly);
 			}
+		}
 
-			if (fenric('event::http.response.before.send.content')->run([$this, & $this->content]))
-			{
-				file_put_contents('php://output', $this->getContent());
-
-				fenric('event::http.response.after.send.content')->run([$this, $this->content]);
-			}
-
-			fenric('event::http.response.after.send')->run([$this]);
+		if ($this->getContent())
+		{
+			echo $this->getContent();
 		}
 
 		/**
@@ -206,52 +376,6 @@ class Response
 		if (function_exists('fastcgi_finish_request'))
 		{
 			fastcgi_finish_request();
-		}
-	}
-
-	/**
-	 * Removes sent header
-	 */
-	public function removeSentHeader(string $name) : void
-	{
-		foreach (headers_list() as $header)
-		{
-			$parts = explode(':', $header, 2);
-
-			list($key, $value) = $parts;
-
-			if (strcasecmp($key, $name) === 0)
-			{
-				header_remove($key);
-
-				break;
-			}
-		}
-	}
-
-	/**
-	 * Removes sent headers
-	 */
-	public function removeSentHeaders() : void
-	{
-		foreach (headers_list() as $header)
-		{
-			$parts = explode(':', $header, 2);
-
-			list($key, $value) = $parts;
-
-			header_remove($key);
-		}
-	}
-
-	/**
-	 * Removes sent output
-	 */
-	public function removeSentOutput() : void
-	{
-		while (ob_get_level() > 0)
-		{
-			ob_end_clean();
 		}
 	}
 }
