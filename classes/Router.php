b9474b77e258aae1ef22acd6b0b8a2de14164868
fenric <afenric@gmail.com>
2018-01-15 01:46:30 +0300

    Обновление копирайтов, централизованное хранение маршрутов, возможность группировки маршрутов, префиксы, неймспейсы, посредники, маршрут по умолчанию, небольшие улучшения.

--- classes/Router.php
+++ classes/Router.php
@@ -2,8 +2,8 @@
 /**
  * It's free open-source software released under the MIT License.
  *
- * @author Anatoly Fenric <a.fenric@gmail.com>
- * @copyright Copyright (c) 2013-2017 by Fenric Laboratory
+ * @author Anatoly Fenric <anatoly.fenric@gmail.com>
+ * @copyright Copyright (c) 2013-2018 by Fenric Laboratory
  * @license https://github.com/fenric/framework/blob/master/LICENSE.md
  * @link https://github.com/fenric/framework
  */
@@ -14,6 +14,8 @@ namespace Fenric;
  * Import classes
  */
 use Closure;
+use ReflectionClass;
+use ReflectionFunction;
 
 /**
  * Router
@@ -24,189 +26,354 @@ class Router
 	/**
 	 * Карта маршрутов
 	 */
-	protected $routes = [];
+	protected $map = [];
 
 	/**
-	 * Изоляционные пространства маршрутов
+	 * Группы маршрутов
 	 */
-	protected $scopes = [];
+	protected $groups = [];
 
 	/**
-	 * Добавление маршрута соответствующего HTTP методу OPTIONS
+	 * Контроллер по умолчанию
 	 */
-	public function options(string $location, string $controller, Closure $eavesdropper = null) : self
+	protected $fallback;
+
+	/**
+	 * Загрузка карты маршрутов
+	 */
+	public function map() : self
 	{
-		return $this->add(['OPTIONS'], $location, $controller, $eavesdropper);
-	}
+		$this->group(function() : void
+		{
+			$this->namespace('\\Fenric\\Controllers\\Api\\');
 
-	/**
-	 * Добавление маршрута соответствующего HTTP методу HEAD
-	 */
-	public function head(string $location, string $controller, Closure $eavesdropper = null) : self
-	{
-		return $this->add(['HEAD'], $location, $controller, $eavesdropper);
-	}
+			$this->prefix('/api');
 
-	/**
-	 * Добавление маршрута соответствующего HTTP методу GET
-	 */
-	public function get(string $location, string $controller, Closure $eavesdropper = null) : self
-	{
-		return $this->add(['GET'], $location, $controller, $eavesdropper);
-	}
+			if (fenric('/routes/api.local.php')->isFile())
+			{
+				require fenric('/routes/api.local.php')->getRealPath();
+			}
+			else if (fenric('/routes/api.php')->isFile())
+			{
+				require fenric('/routes/api.php')->getRealPath();
+			}
+			else if (fenric('/routes/api.example.php')->isFile())
+			{
+				require fenric('/routes/api.example.php')->getRealPath();
+			}
+		});
 
-	/**
-	 * Добавление маршрута соответствующего HTTP методу POST
-	 */
-	public function post(string $location, string $controller, Closure $eavesdropper = null) : self
-	{
-		return $this->add(['POST'], $location, $controller, $eavesdropper);
-	}
+		$this->group(function() : void
+		{
+			$this->namespace('\\Fenric\\Controllers\\');
 
-	/**
-	 * Добавление маршрута соответствующего HTTP методу PATCH
-	 */
-	public function patch(string $location, string $controller, Closure $eavesdropper = null) : self
-	{
-		return $this->add(['PATCH'], $location, $controller, $eavesdropper);
-	}
-
-	/**
-	 * Добавление маршрута соответствующего HTTP методу DELETE
-	 */
-	public function delete(string $location, string $controller, Closure $eavesdropper = null) : self
-	{
-		return $this->add(['DELETE'], $location, $controller, $eavesdropper);
-	}
-
-	/**
-	 * Добавление маршрута соответствующего HTTP методу PUT
-	 */
-	public function put(string $location, string $controller, Closure $eavesdropper = null) : self
-	{
-		return $this->add(['PUT'], $location, $controller, $eavesdropper);
-	}
-
-	/**
-	 * Добавление маршрута соответствующего безопасному HTTP методу
-	 */
-	public function safe(string $location, string $controller, Closure $eavesdropper = null) : self
-	{
-		return $this->add(['GET', 'POST'], $location, $controller, $eavesdropper);
-	}
-
-	/**
-	 * Добавление маршрута соответствующего любому HTTP методу
-	 */
-	public function any(string $location, string $controller, Closure $eavesdropper = null) : self
-	{
-		$methods = ['OPTIONS', 'HEAD', 'GET', 'POST', 'PATCH', 'DELETE', 'PUT'];
-
-		return $this->add($methods, $location, $controller, $eavesdropper);
-	}
-
-	/**
-	 * Добавление маршрута
-	 */
-	public function add(array $methods, string $location, string $controller, Closure $eavesdropper = null) : self
-	{
-		$this->routes[] = [$methods, implode($this->scopes) . $location, $controller, $eavesdropper];
+			if (fenric('/routes/web.local.php')->isFile())
+			{
+				require fenric('/routes/web.local.php')->getRealPath();
+			}
+			else if (fenric('/routes/web.php')->isFile())
+			{
+				require fenric('/routes/web.php')->getRealPath();
+			}
+			else if (fenric('/routes/web.example.php')->isFile())
+			{
+				require fenric('/routes/web.example.php')->getRealPath();
+			}
+		});
 
 		return $this;
 	}
 
 	/**
-	 * Установка изоляционного пространства для маршрутов
+	 * Установка контроллера для домашнего адреса
 	 */
-	public function scope($scope, Closure $callback) : void
+	public function home($controller) : self
 	{
-		$this->scopes[] = $scope;
-
-		$callback($this);
-
-		array_pop($this->scopes);
+		return $this->get('/', $controller);
 	}
 
 	/**
-	 * Определение маршрута соответствующего текущему HTTP запросу
+	 * Установка контроллера по умолчанию
 	 */
-	public function match(Request $request)
+	public function fallback($controller) : self
 	{
-		if (count($this->routes) > 0)
+		$this->fallback = $controller;
+
+		return $this;
+	}
+
+	/**
+	 * Добавление маршрута соответствующего HTTP методу OPTIONS
+	 */
+	public function options(string $location, $controller) : self
+	{
+		return $this->add(['OPTIONS'], $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута соответствующего HTTP методу HEAD
+	 */
+	public function head(string $location, $controller) : self
+	{
+		return $this->add(['HEAD'], $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута соответствующего HTTP методу GET
+	 */
+	public function get(string $location, $controller) : self
+	{
+		return $this->add(['GET'], $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута соответствующего HTTP методу POST
+	 */
+	public function post(string $location, $controller) : self
+	{
+		return $this->add(['POST'], $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута соответствующего HTTP методу PATCH
+	 */
+	public function patch(string $location, $controller) : self
+	{
+		return $this->add(['PATCH'], $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута соответствующего HTTP методу DELETE
+	 */
+	public function delete(string $location, $controller) : self
+	{
+		return $this->add(['DELETE'], $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута соответствующего HTTP методу PUT
+	 */
+	public function put(string $location, $controller) : self
+	{
+		return $this->add(['PUT'], $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута соответствующего безопасному HTTP методу
+	 */
+	public function safe(string $location, $controller) : self
+	{
+		return $this->add(['GET', 'POST'], $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута соответствующего любому HTTP методу
+	 */
+	public function any(string $location, $controller) : self
+	{
+		$methods = ['OPTIONS', 'HEAD', 'GET', 'POST', 'PATCH', 'DELETE', 'PUT'];
+
+		return $this->add($methods, $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута соответствующего CRUD подходу
+	 */
+	public function crud(string $location, $controller) : self
+	{
+		$methods = ['GET', 'POST', 'PATCH', 'DELETE'];
+
+		return $this->add($methods, $location, $controller);
+	}
+
+	/**
+	 * Добавление маршрута
+	 */
+	public function add(array $methods, string $location, $controller) : self
+	{
+		end($this->groups);
+
+		$key = key($this->groups);
+
+		reset($this->groups);
+
+		if (isset($this->groups[$key]['prefix']))
 		{
-			foreach ($this->routes as $route)
+			$location = $this->groups[$key]['prefix'] . $location;
+		}
+
+		if (isset($this->groups[$key]['namespace']) && is_string($controller))
+		{
+			$controller = $this->groups[$key]['namespace'] . $controller;
+		}
+
+		if (isset($this->groups[$key]['middleware']))
+		{
+			$middleware = $this->groups[$key]['middleware'];
+		}
+
+		$this->map[] = [
+			'methods' => $methods,
+			'location' => $location,
+			'controller' => $controller,
+			'middleware' => $middleware ?? function() {
+				return true;
+			},
+		];
+
+		return $this;
+	}
+
+	/**
+	 * Создание группы маршрутов
+	 */
+	public function group(Closure $callback) : void
+	{
+		$last = [];
+
+		if (count($this->groups) > 0)
+		{
+			$last = end($this->groups);
+
+			reset($this->groups);
+		}
+
+		$this->groups[] = $last;
+
+		$callback();
+
+		array_pop($this->groups);
+	}
+
+	/**
+	 * Добавление префикса для группы маршрутов
+	 */
+	public function prefix(string $prefix) : self
+	{
+		end($this->groups);
+
+		$key = key($this->groups);
+
+		reset($this->groups);
+
+		if (isset($this->groups[$key]['prefix']))
+		{
+			$prefix = $this->groups[$key]['prefix'] . $prefix;
+		}
+
+		$this->groups[$key]['prefix'] = $prefix;
+
+		return $this;
+	}
+
+	/**
+	 * Добавление пространства имен для группы маршрутов
+	 */
+	public function namespace(string $namespace) : self
+	{
+		end($this->groups);
+
+		$key = key($this->groups);
+
+		reset($this->groups);
+
+		if (isset($this->groups[$key]['namespace']))
+		{
+			$namespace = $this->groups[$key]['namespace'] . $namespace;
+		}
+
+		$this->groups[$key]['namespace'] = $namespace;
+
+		return $this;
+	}
+
+	/**
+	 * Добавление промежуточной логики для группы маршрутов
+	 */
+	public function middleware(Closure $middleware) : self
+	{
+		end($this->groups);
+
+		$key = key($this->groups);
+
+		reset($this->groups);
+
+		if (isset($this->groups[$key]['middleware']))
+		{
+			$previous = $this->groups[$key]['middleware'];
+
+			$middleware = function(...$args) use($previous, $middleware) : bool
 			{
-				list($methods, $location, $controller, $eavesdropper) = $route;
+				return $previous(...$args) && $middleware(...$args);
+			};
+		}
 
-				if (in_array($request->getMethod(), $methods))
+		$this->groups[$key]['middleware'] = $middleware;
+
+		return $this;
+	}
+
+	/**
+	 * Запуск маршрутизатора
+	 */
+	public function run(Request $request = null, Response $response = null) : bool
+	{
+		$request = $request ?: fenric('request');
+		$response = $response ?: fenric('response');
+
+		if (count($this->map) > 0)
+		{
+			foreach ($this->map as $route)
+			{
+				if (in_array($request->method(), $route['methods']))
 				{
-					if ($location = parse_url($location))
-					{
-						if (empty($location['host']) || strcmp($request->getHost(), $location['host']) === 0)
-						{
-							if (empty($location['port']) || strcmp($request->getPort(), $location['port']) === 0)
-							{
-								$route['expression'] = $this->convertRoutePathToRegularExpression($request->getRoot() . $location['path'] ?? '/');
+					$route['regexp'] = $this->convertRoutePathToRegularExpression($request->root() . $route['location']);
 
-								if (preg_match($route['expression'], $request->getPath(), $parameters))
-								{
-									return [
-										'route' => $route,
-										'controller' => $controller,
-										'eavesdropper' => $eavesdropper,
-										'parameters' => array_filter($parameters),
-									];
-								}
+					if (preg_match($route['regexp'], $request->path(), $route['parameters']))
+					{
+						$request->parameters->upgrade($route['parameters'])->filter();
+
+						if ($route['middleware']($request, $response))
+						{
+							if ($this->execute($route['controller'], $request, $response))
+							{
+								return true;
 							}
 						}
 					}
 				}
 			}
 		}
+
+		if ($this->execute($this->fallback, $request, $response))
+		{
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
-	 * Запуск маршрутизатора
+	 * Выполнение контроллера
 	 */
-	public function run(Request $request, Response $response, Closure $digression = null) : bool
+	protected function execute($controller, Request $request, Response $response) : bool
 	{
-		$response->setStatus(404);
-
-		fenric('event::router.runned')->run(
-			[$this, $request, $response]
-		);
-
-		if ($match = $this->match($request))
+		if (is_string($controller))
 		{
-			fenric('event::router.matched')->run(
-				[$this, $request, $response, & $match]
-			);
-
-			if (is_string($match['controller']))
+			if (class_exists($controller))
 			{
-				if (class_exists($match['controller']))
+				$reflector = new ReflectionClass($controller);
+
+				if ($reflector->isInstantiable())
 				{
-					if (is_subclass_of($match['controller'], Controller::class))
+					if ($reflector->isSubclassOf(Controller::class))
 					{
-						$request->parameters->update($match['parameters']);
+						$instance = $reflector->newInstance($request, $response);
 
-						$response->setStatus(200);
-
-						$controller = new $match['controller']($this, $request, $response);
-
-						fenric('event::router.eavesdropping')->run(
-							[$this, $request, $response, $controller, $match]
-						);
-
-						if ($match['eavesdropper'] instanceof Closure)
+						if ($instance->preInit())
 						{
-							$match['eavesdropper']($this, $request, $response, $controller, $match);
-						}
-
-						if ($controller->preInit())
-						{
-							$controller->init();
-							$controller->render();
+							$instance->init();
+							$instance->render();
 
 							return true;
 						}
@@ -215,13 +382,18 @@ class Router
 			}
 		}
 
-		fenric('event::router.digression')->run(
-			[$this, $request, $response]
-		);
-
-		if ($digression instanceof Closure)
+		if (is_callable($controller))
 		{
-			$digression($this, $request, $response);
+			$reflector = new ReflectionFunction($controller);
+
+			if ($reflector->isUserDefined())
+			{
+				$callback = $reflector->getClosure();
+
+				$callback($request, $response);
+
+				return true;
+			}
 		}
 
 		return false;
@@ -241,7 +413,7 @@ class Router
 
 		$routePath = addcslashes($routePath, '\.+?[^]${}=!|:-#');
 
-		$routePath = str_replace(['(', '*', ')'], ['(?:', '.*', ')?'], $routePath);
+		$routePath = str_replace(['(', '*', '%', ')'], ['(?:', '[^/]*', '.*', ')?'], $routePath);
 
 		$routePath = preg_replace_callback('/<(\w+)>/', $creatingSubpatternsOfParameters, $routePath);
 
@@ -255,7 +427,7 @@ class Router
 	{
 		$extractedExpressions = [];
 
-		$expressionForSearchParameters = '/<(\w+)(?::([^>]+))?>/';
+		$expressionForSearchParameters = '/<(\w+)(?::([^<>]+))?>/';
 
 		if (preg_match_all($expressionForSearchParameters, $routePath, $matches, PREG_SET_ORDER))
 		{
